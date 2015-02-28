e7073c1dd0ecafe9ca2e0a6fd514f3565059d9a1
Logan Graba <logangraba@gmail.com>
2015-02-27 18:26:53 -0700

    Devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,15 @@ Rails.application.configure do
 
   # Mailer Settings
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+#  config.action_mailer.raise_delivery_errors = true
+#  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.smtp_settings = {
+#  address: “smtp.mailgun.org”,
+#  port: 587,
+#  domain: ENV[“MAILGUN_DOMAIN”],
+#  authentication: “plain”,
+#  enable_starttls_auto: true,
+#  user_name: ENV[“MAILGUN_USERNAME”],
+#  password: ENV[“MAILGUN_PASSWORD”]
+#  }
 end
